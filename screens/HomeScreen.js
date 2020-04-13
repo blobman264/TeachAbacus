eb5384175b39ddf9493b1f7560e01685918c4fde
blobman264 <32684802+blobman264@users.noreply.github.com>
2020-04-13 13:23:12 +0100

    test

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -41,7 +41,7 @@ render(){
       data={this.state.dataSource}
       renderItem={({ item }) => (
         <Text>
-          {item.id}, {item.title}
+          {item.id}, {item.first_name}
         </Text>
       )}
       keyExtractor={({ id }, index) => id}
